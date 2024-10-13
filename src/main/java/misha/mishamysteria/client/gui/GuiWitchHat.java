15405b940b99b2666f95713df9c4bacb162b31db
Cirno <9@korewa.li>
2024-10-13 09:55:16 +0700

    feat: locale witch hat GUI

--- src/main/java/misha/mishamysteria/client/gui/GuiWitchHat.java
+++ src/main/java/misha/mishamysteria/client/gui/GuiWitchHat.java
@@ -41,7 +41,7 @@ public class GuiWitchHat extends GuiContainer {
      */
     protected void drawGuiContainerForegroundLayer(int mouseX, int mouseY)
     {
-        this.fontRenderer.drawString("Hat container", 8, 6, 4210752);
+        this.fontRenderer.drawString(I18n.format("gui.misha.witch_hat.inv"), 8, 6, 4210752);
         this.fontRenderer.drawString(this.playerInventory.getDisplayName().getUnformattedText(), 8, this.ySize - 96 + 2, 4210752);
     }
 
